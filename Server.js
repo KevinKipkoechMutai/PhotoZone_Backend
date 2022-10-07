9c34fd754e26ab87ba13a076195c709d747268dd
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-07 06:47:10 +0300

    adding server files

--- Server.js
+++ Server.js
@@ -1,13 +1,21 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./db.json')
+const router = jsonServer.router('db.json')
+const path = require('path')
+const express = require('express')
+
 
 const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-server.use(router)
+
+server.use('/db', middlewares, router)
+server.use(express.static(path.join(__dirname, 'build')))
 
 const port = process.env.PORT || 3000
+
+server.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`)
